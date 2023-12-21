ca120b77724e1ab12a635057d475b0d818cbaaa5
JorgeLopes-BytePitch <jorge.lopes@bytepitch.com>
2023-12-21 11:44:20 +0000

    last updates

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # arbitrage-bot-test-env
+
+## Install bundle
+
+agd tx swingset install-bundle @/workspace/bundles/asset-faucet/b1-bc90824909ce251e1c1647c8348fcac80bc49fc240af2c116ea0a6f4e289090f650812c1fa4c770bc5e10e6ec5926efebed9ec95d302620f51aa5e9443f9c694.json --from=gov1  --chain-id=agoriclocal --node=http://0.0.0.0:26657 --keyring-backend=test --gas=auto --yes -b block
+
+### Error message
+
+raw_log: 'controller refused message admission: 0uist is smaller than 1289800000uist:
+  insufficient funds: mempool is full'
+
+
+## Provision account
+
+ACCT_ADDR=agoric1mcm0ffsh0a20hlzgx5wylzw0sm85hxx05azsxx FUNDS=9999000000ubld,9999000000uist make fund-acct
--- scripts/commands.js
+++ scripts/commands.js
@@ -3,9 +3,16 @@ import { readFileSync } from "fs";
 
 const agdBin = "agd";
 
-const GLOBAL_OPTIONS = [
-  // '--keyring-backend=test',
-  "--output=json",
+const GLOBAL_OPTIONS = ["--keyring-backend=test", "--output=json"];
+
+const SIGN_BROADCAST_OPTS = [
+  "--keyring-backend=test",
+  "--gas=auto",
+  "--fees=80000ubld",
+  "--gas-adjustment=1.2",
+  "--yes",
+  "--output",
+  "json",
 ];
 
 const agd = {
@@ -27,74 +34,77 @@ const agd = {
         ].join(" "),
     },
   },
-  core_eval: {
-    publish: (bundle, params) =>
-      [
-        agdBin,
-        "tx",
-        "swingset",
-        "install-bundle",
-        bundle,
-        "--node",
-        params.rpc,
-        "--from",
-        params.key,
-        "--chain-id",
-        params.chain_id,
-        "--keyring-backend=test",
-        "--gas",
-        params.gas,
-        "-b block",
-        "--yes",
-      ].join(" "),
+  tx: {
+    swingset: {
+      publish: (bundle, params) =>
+        [
+          agdBin,
+          "tx",
+          "swingset",
+          "install-bundle",
+          bundle,
+          "--node",
+          params.rpc,
+          "--from",
+          params.key,
+          "--chain-id",
+          params.chain_id,
+          "--keyring-backend=test",
+          "--gas",
+          params.gas,
+          "-b block",
+          "--yes",
+        ].join(" "),
+    },
+    gov: {
+      submit: (coreList, params) =>
+        [
+          agdBin,
+          "tx",
+          "gov",
+          "submit-proposal",
+          "swingset-core-eval",
+          ...coreList,
+          "--title",
+          params.title,
+          "--description",
+          params.description,
+          "--deposit",
+          params.deposit,
+          "--from",
+          params.key,
+          "--chain-id",
+          params.chain_id,
+          "--keyring-backend=test",
+          "--gas-adjustment",
+          params.adjustment,
+          "--gas",
+          params.gas,
+          "-b block",
+          "--yes",
+        ].join(" "),
 
-    submit: (coreList, params) =>
-      [
-        agdBin,
-        "tx",
-        "gov",
-        "submit-proposal",
-        "swingset-core-eval",
-        ...coreList,
-        "--title",
-        params.title,
-        "--description",
-        params.description,
-        "--deposit",
-        params.deposit,
-        "--from",
-        params.key,
-        "--chain-id",
-        params.chain_id,
-        "--keyring-backend=test",
-        "--gas-adjustment",
-        params.adjustment,
-        "--gas",
-        params.gas,
-        "-b block",
-        "--yes",
-      ].join(" "),
-
-    vote: (proposal_id, params) =>
-      [
-        agdBin,
-        "tx",
-        "gov",
-        "vote",
-        proposal_id,
-        "yes",
-        "--from",
-        params.key,
-        "--chain-id",
-        params.chain_id,
-        "--keyring-backend=test",
-        "--gas-adjustment",
-        params.adjustment,
-        "--gas",
-        params.gas,
-        "-b block",
-        "--yes",
-      ].join(" "),
+      vote: (proposal_id, params) =>
+        [
+          agdBin,
+          "tx",
+          "gov",
+          "vote",
+          proposal_id,
+          "yes",
+          "--from",
+          params.key,
+          "--chain-id",
+          params.chain_id,
+          "--keyring-backend=test",
+          "--gas-adjustment",
+          params.adjustment,
+          "--gas",
+          params.gas,
+          "-b block",
+          "--yes",
+        ].join(" "),
+    },
   },
 };
 
@@ -128,7 +138,7 @@ const spreadList = (list) => {
 
 const publishContract = (bundle, params) => {
   console.log("Publish contract");
-  execute(agd.core_eval.publish(bundle, params), {
+  execute(agd.tx.swingset.publish(bundle, params), {
     stdio: "pipe",
   });
   console.log("Success");
@@ -137,7 +147,7 @@ const publishContract = (bundle, params) => {
 const submitCoreEval = (list, params) => {
   console.log("Submitting core-eval");
   const coreList = spreadList(list);
-  execute(agd.core_eval.submit(coreList, params), {
+  execute(agd.tx.gov.submit(coreList, params), {
     stdio: "pipe",
   });
   console.log("Success");
@@ -147,13 +157,11 @@ const vote = (params) => {
   console.log("Vote on proposal");
   const proposal = queryProposals(params.rpc);
   const proposal_id = proposal[0].proposal_id;
-  execute(agd.core_eval.vote(proposal_id, params), {
+  execute(agd.tx.gov.vote(proposal_id, params), {
     stdio: "pipe",
   });
   console.log("Success");
 };
-
-console.log(queryProposals("http://0.0.0.0:26657"));
 
 export {
   agd,
